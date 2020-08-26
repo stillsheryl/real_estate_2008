fac38d9e0b466104d554f608cef62b8cabfc7449
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-26 09:26:04 -0600

    Test it has attributes: price and address

--- test/house_test.rb
+++ test/house_test.rb
@@ -11,6 +11,17 @@ class HouseTest < Minitest::Test
     assert_instance_of House, house
   end
 
+  def test_it_has_a_price
+    house = House.new("$400000", "123 sugar lane")
 
+    assert_equal "$400000", house.price
+  end
+
+  def test_it_has_an_address
+    house = House.new("$400000", "123 sugar lane")
+
+    assert_equal "123 sugar lane", house.address
+
+  end
 
 end
