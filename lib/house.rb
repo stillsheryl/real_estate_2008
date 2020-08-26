1bff241cc20584d399b3d449d3943f5359c02a04
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-26 10:15:31 -0600

    Add calculation for house area

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,10 @@
 class House
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :total_area
   def initialize(price, address)
     @price = price
     @address = address
     @rooms = []
+    @total_area = 0
   end
 
   def add_room(room)
@@ -18,6 +19,13 @@ class House
     @rooms.find_all do |room|
       room.category == category
     end
+  end
+
+  def area
+    @rooms.each do |room|
+      @total_area += room.area.to_i
+    end
+    total_area
   end
 
 end
