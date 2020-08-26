680b8959312157e6479f1b160e2fdad4f4762a81
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-26 09:31:02 -0600

    Add test for ability to add rooms to house

--- test/house_test.rb
+++ test/house_test.rb
@@ -29,4 +29,15 @@ class HouseTest < Minitest::Test
     assert_equal [], house.rooms
   end
 
+  def test_can_add_rooms_to_house
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    assert_equal [], house.rooms
+    house.add_room(room_1)
+    house.add_room(room_2)
+    assert_equal [room1, room2], house.rooms
+  end
+
 end
