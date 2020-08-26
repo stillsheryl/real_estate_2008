430553c403c58552a2f91e40b5c0627a91f859b4
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-26 09:23:20 -0600

    Add tests that house class exists

--- test/house_test.rb
+++ test/house_test.rb
@@ -5,7 +5,11 @@ require './lib/house'
 
 class HouseTest < Minitest::Test
 
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
 
+    assert_instance_of House, house
+  end
 
 
 
