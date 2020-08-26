0271ed8f08bb6d9c0f44db1e9551429dd955a3ce
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-08-26 10:38:42 -0600

    Add test to sort rooms by square feet

--- test/house_test.rb
+++ test/house_test.rb
@@ -103,4 +103,18 @@ class HouseTest < Minitest::Test
     assert_equal 210.53, house.price_per_square_foot
   end
 
+  def test_can_sort_rooms_by_are
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_4)
+    house.add_room(room_1)
+    house.add_room(room_3)
+    house.add_room(room_2)
+
+    assert_equal [room_4, room_3, room_2, room_1], house.rooms_sorted_by_area
+  end
+
 end
